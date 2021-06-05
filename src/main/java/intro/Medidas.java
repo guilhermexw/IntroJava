4e0b46174bc732b48f89fa4aac79aac8611233c8
guilherme.wichroski <guilhermexw@gmail.com>
2021-06-05 12:01:39 -0300

    Metodo IF

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -9,18 +9,26 @@ public class Medidas {
 
     // 3.2 - Métodos e Funções
     public static void main(String[] args){
-        calcularAreaModoCompacto();
+
+        int modo = 1;
+
+        if (modo == 1){
+            calcularAreaModoCompacto();
+        }
+        else {
+            calcularAreaModoExtenso();
+        }
 
     }
 
-    public void calcularAreaModoExtenso(){
+    public static void calcularAreaModoExtenso(){
         // Calculo de área - Exemplo: o tamanho do tapete ou do piso
         int largura;
         int comprimento;
         int resultado;
 
         largura = 5; // largura recebe 4
-        comprimento = 6;
+        comprimento = 10;
 
         resultado = largura * comprimento;
         System.out.println("Para a largura de " + largura + "m e o comprimento de "
